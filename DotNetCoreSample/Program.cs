a9ce7fa4b9a94e919909b1c49be83fec91c2e693
ASanklecha <ASanklecha@americancampus.com>
2018-01-02 13:22:43 -0600

    Modified messages

--- DotNetCoreSample/Program.cs
+++ DotNetCoreSample/Program.cs
@@ -6,7 +6,7 @@ namespace DotNetCoreSample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World from DotNetCore!");           
+            Console.WriteLine("Hello World from DotNetCore!!!");           
         }
     }
 }
